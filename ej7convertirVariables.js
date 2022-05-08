8cb462a3bed469ee602d696fc1281e061ac9d2d5
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 21:19:01 -0500

    Update ej7convertirVariables.js

--- ej7convertirVariables.js
+++ ej7convertirVariables.js
@@ -1,18 +1,14 @@
-let valor = 10; //solo cambiar este valor 
-
+//? convertir variables entre numerica y cadenas
+let valor = 10; //solo cambiar este valor. cambia de numerica a cadena.
 console.log("Valor Original: ", valor, typeof valor);
+console.log();
 console.log("Cambio a cadena carracteres");
-function cadena (m){ 
-    return console.log(String(m), typeof String(m))
-};
+function cadena (m){ return console.log(String(m), typeof String(m)) };
 cadena(valor);
 console.log();
-//------------------------------------------
-let valorN = "20"; //solo cambiar este valor 
-
+// parte 2
+let valorN = "20"; //solo cambiar este valor. cambia de cadena a numerica.
 console.log("Valor Original: ", valorN, typeof valorN);
 console.log("Cambio a Numerico");
-function cadenaN (n){ return console.log(Number(n), typeof Number(n))};
+function cadenaN (n){ return console.log(Number(n), typeof Number(n)) };
 cadenaN(valorN);
-console.log();
-
