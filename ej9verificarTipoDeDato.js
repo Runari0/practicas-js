e0d19ce564295de5d3b2ea43a6ee3927560a05a1
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 21:40:48 -0500

    Update ej9verificarTipoDeDato.js
    muestra que dato que esta colocado

--- ej9verificarTipoDeDato.js
+++ ej9verificarTipoDeDato.js
@@ -1,24 +1,24 @@
-function caracter(n){
-if (typeof n === "string"){
-    return console.log(`Valor ${n} es una cadena de caracteres`);
-}else if(typeof n === "number"){
-    return console.log(`Valor ${n} es una numero`);
-}else if(typeof n === "boolean"){
-    return console.log(`Valor ${n} es un boolean`);
-}else if(typeof n === "NaN"){
-    return console.log(`Valor ${n} es un NaN`);
-}else if(typeof n === "undefined")
-{    return console.log(`Valor ${n} es un undefined`);
-}else if(typeof n === "esFinite"){
-    return console.log(`Valor ${n} es un Finite`);
-}else{
-    return console.log(`Valor introducido es '${n}' de tipo ${typeof n}`);
-}
-}
-caracter(Date());
-//----------------------------------------------
-console.log();
-console.log("Segunda forma");
-//--------------------------------------
+//? muestraque dato que esta colocado 
+//? form 1
 function caracte(n){ return console.log(`Valor introducido '${n}' es de tipo ${typeof n}`)};
-caracte(Date());
+caracte(Date());// cambia este valor
+//? forma 2
+console.log()
+function caracter(n){
+    if (typeof n === "string"){
+        return console.log(`Valor introducido ${n} es una cadena de caracteres`);
+    }else if(typeof n === "number"){
+        return console.log(`Valor introducido ${n} es una numero`);
+    }else if(typeof n === "boolean"){
+        return console.log(`Valor introducido ${n} es un boolean`);
+    }else if(typeof n === "NaN"){
+        return console.log(`Valor introducido ${n} es un NaN`);
+    }else if(typeof n === "undefined"){
+        return console.log(`Valor introducido ${n} es un undefined`);
+    }else if(typeof n === "esFinite"){
+    return console.log(`Valor introducido ${n} es un Finite`);
+    }else{
+    return console.log(`Valor introducido es '${n}' de tipo ${typeof n}`);
+    }
+}
+caracter(Date());// cambia este valor
