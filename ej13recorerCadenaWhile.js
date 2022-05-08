df4e275e8c06d6b0913e022ebb7db77499078047
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 19:57:19 -0500

    Update ej13recorerCadenaWhile.js

--- ej13recorerCadenaWhile.js
+++ ej13recorerCadenaWhile.js
@@ -1,22 +1,20 @@
-const arr = [10, 9, 8, 7, 6];
+//? recorer cadena con while
 function Recorrer(arr){
 i = 0;
-while( i < arr.length) {
-console.log(arr[i]); 
+    while( i < arr.length) {
+    console.log(arr[i]); 
     i++
+    }
 }
-}
-
-Recorrer(arr);
+Recorrer([10, 9, 8, 7, 6]);
 
 console.log()
-console.log("reversa")
-function RecorrerR(arr){
-    i = arr.length;
+//?reversa
+function reversa(arr){
+i = arr.length;
     while( i >= 0) {
     console.log(arr[i]); 
-        i--
+    i--
     }
-    }
-    
-    RecorrerR(arr);
+}
+reversa([10, 9, 8, 7, 6]);
