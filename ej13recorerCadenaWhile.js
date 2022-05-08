f22a2b5ce63f2e92ad3feb842a4edfdf46266a09
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 20:22:08 -0500

    Update ej13recorerCadenaWhile.js

--- ej13recorerCadenaWhile.js
+++ ej13recorerCadenaWhile.js
@@ -3,17 +3,19 @@ function Recorrer(arr){
 i = 0;
     while( i < arr.length) {
     console.log(arr[i]); 
-    i++
+    i++;
     }
 }
 Recorrer([10, 9, 8, 7, 6]);
-console.log()
+
+console.log();
 //?reversa
+console.log("reversa");
 function reversa(arr){
 i = arr.length;
     while( i >= 0) {
     console.log(arr[i]); 
-    i--
+    i--;
     }
 }
 reversa([10, 9, 8, 7, 6]);
