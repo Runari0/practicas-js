faae36e647695443641eab6db5d207f15c5f700f
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 19:48:21 -0500

    Update ej12conteoCadena.js

--- ej12conteoCadena.js
+++ ej12conteoCadena.js
@@ -1,25 +1,25 @@
+//?cuenta lso elementos de la cadena
+//? form 1
 function conteoRecursivo(texto) {
     return texto.length ? 1 + conteoRecursivo(texto.slice(1)) : 0;
 }
-
 function conteo(texto) {
     return (typeof texto != 'string') ? console.error('El argumento debe ser una cadena de caracteres.') : console.log(`la cadena tiene ${conteoRecursivo(texto)} elements`);
 }
+conteo("cinco");
 
-conteo("aaa");
-
-// function conteoRecursivo(texto) {
-//     if (texto.length > 0){
-//         return 1 + conteoRecursivo(texto.slice(1))
-//     }else{
-//         return 0;
-//     }
-// }
-
-// function conteo(texto) {
-//     if (typeof texto != 'string') {
-//         return console.error('El argumento debe ser una cadena de caracteres.');
-//     }
-//     return console.log(`la cadena tiene ${conteoRecursivo(texto)} elements`);
-// }
-// conteo();
+//? form 2
+function conteoRecursivo2(texto) {
+    if (texto.length > 0){
+        return 1 + conteoRecursivo2(texto.slice(1))
+    }else{
+        return 0;
+    }
+}
+function conteo2(texto) {
+    if (typeof texto != 'string') {
+        return console.error('El argumento debe ser una cadena de caracteres.');
+    }
+    return console.log(`la cadena tiene ${conteoRecursivo2(texto)} elements`);
+}
+conteo2();
