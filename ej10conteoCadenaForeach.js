2e2f623643ccd564a752b81986796f1f9295d617
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 19:32:14 -0500

    Update ej10conteoCadenaForeach.js

--- ej10conteoCadenaForeach.js
+++ ej10conteoCadenaForeach.js
@@ -1,22 +1,19 @@
 //? funcion para contar numerando los elemento de la cadena, asi como mostrandolos.
+
 //? forma 1
-function recorridoArreglo(valor, indice, lenguajes) {
-    console.log(`${valor} se encuentra en el índice ${indice}`);
-    
+function recorridoArreglo(valor, indice, lenguajes) { 
+    console.log(`${valor} se encuentra en el índice ${indice}`);   
 }
 function recorrido(cadena){
     cadena.forEach(recorridoArreglo);
 }
-
 recorrido(["ana","karen",])
-
 
 // forma 2
 function conteo(n){
     let lenguajes = n;
-lenguajes.forEach(function(valor, indice, lenguajes) {
-    console.log(`${valor} se encuentra en el índice ${indice}`);
-});
+    lenguajes.forEach(function(valor, indice, lenguajes) {
+        console.log(`${valor} se encuentra en el índice ${indice}`);
+    });
 }
-
 conteo(["ana","karen","lol"])
