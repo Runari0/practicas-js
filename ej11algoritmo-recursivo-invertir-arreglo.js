5cec86675f1b3671a68326f834658a25136b2db9
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 19:42:23 -0500

    Update ej11algoritmo-recursivo-invertir-arreglo.js

--- ej11algoritmo-recursivo-invertir-arreglo.js
+++ ej11algoritmo-recursivo-invertir-arreglo.js
@@ -1,21 +1,24 @@
-// function invertirArregloProceso(arreglo, indice, cadena){
-//     return indice == 0 ? cadena : invertirArregloProceso(arreglo, --indice, (cadena += " " + arreglo[indice]));
-// }
-
-
+// invertir arreglo con un algoritmo recursivo
+//forma 1
 function invertirArregloProceso(arreglo, indice, cadena){
+    return indice == 0 ? cadena : invertirArregloProceso(arreglo, --indice, (cadena += " " + arreglo[indice]));
+}
+function invertirArreglo(nombres){
+    let resultado = invertirArregloProceso(nombres, nombres.length, ``);
+    console.log(resultado)
+}
+invertirArreglo(["ana","maria","luis","z"])
+console.log();
+//forma 2
+function invertirArregloProceso2(arreglo, indice, cadena){
     if (indice == 0){
         return cadena;
     }else{
-        return invertirArregloProceso(arreglo, --indice, (cadena += " " + arreglo[indice]));
+        return invertirArregloProceso2(arreglo, --indice, (cadena += " " + arreglo[indice]));
     }
 }
-
-function invertirArreglo(nombres){
-let resultado = invertirArregloProceso(nombres, nombres.length, ``);
-console.log(resultado)
+function invertirArreglo2(nombres){
+    let resultado = invertirArregloProceso2(nombres, nombres.length, ``);
+    console.log(resultado)
 }
-
-invertirArreglo(["ana","maria","luis","z"])
-
-
+invertirArreglo2(["ana","maria","luis","z"]);
