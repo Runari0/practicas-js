9c2a8c514ba9808328e917e066c919431fe7f2c1
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 20:01:07 -0500

    Update ej14recorrerCadenaforOf.js

--- ej14recorrerCadenaforOf.js
+++ ej14recorrerCadenaforOf.js
@@ -1,5 +1,3 @@
+//? recorrer la cadena con for of
 let animales = ['gallo🐔', 'cerdo🐷', 'oveja🐑', 'conejo🐇'];
-
-for (let animal of animales){
-    console.log(animal);
-}
+for (let animal of animales){ console.log(animal);}
