cdc1f1075f14e426ed329e4ed593002cd2cf8d95
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 20:53:31 -0500

    Update ej4par-inpar.js

--- ej4par-inpar.js
+++ ej4par-inpar.js
@@ -1,18 +1,24 @@
+//? funcion para detectar par Y impar
+//? form 1 operador condicional
 function parImpar(n){
-if (typeof n ==='number'){
-    if (n === 0 ) {
-        console.log("Par");
-    }else if(n === 1){
-        console.log("Impar")
-    }else {
-    return parImpar(n -2);
-    }
-}else{ return console.error(`el valor entrante ${n} no es numerico`)}
+    return (typeof n ==='number') ? [(n === 0 ) ? console.log("Par") : (n === 1) ? console.log("Impar") : parImpar(n -2)] 
+    : console.error(`el valor entrante ${n} no es numerico`);
 }
-parImpar(10)
+parImpar(10);
 
-// function parImpar(n){
-//     return (typeof n ==='number') ? [(n === 0 ) ? console.log("Par") : (n === 1) ? console.log("Impar") : parImpar(n -2)] 
-//     : console.error(`el valor entrante ${n} no es numerico`);
-// }
-// parImpar(10);
+//? forma 2
+function parImpar2(n){
+    if (typeof n ==='number'){
+        if (n === 0 ) {
+        console.log("Par");
+        }else if(n === 1){
+        console.log("Impar");
+        }else {
+        return parImpar2(n -2);
+        }
+    }else
+    { 
+        return console.error(`el valor entrante ${n} no es numerico`);
+    }
+}
+parImpar2(11);
