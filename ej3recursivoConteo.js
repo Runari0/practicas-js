79e09afa29b5a5da5d91ea910a9dd06f6b995f76
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 20:46:37 -0500

    Update ej3recursivoConteo.js

--- ej3recursivoConteo.js
+++ ej3recursivoConteo.js
@@ -1,10 +1,9 @@
+//? cuanta atras funcion recursivo 
 function cuentaAtras(n){
     if (n === 0){
-        return console.log(n);//console.log(n) lo use para que tome el 0 en el conteo
+        return console.log(n); //console.log(n) lo use para que tome el 0 en el conteo
     }
     console.log(n);
-    return cuentaAtras(n -1)//recursivo
+    return cuentaAtras(n -1) //recursivo
 };
-cuentaAtras(100)
-// colocarlo en una linea
-
+cuentaAtras(100); // colocarlo en una linea
