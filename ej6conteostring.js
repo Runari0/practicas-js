f7f0a32f491abaa3bfce47e4a70a14404776f80f
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 21:08:36 -0500

    Update ej6conteostring.js

--- ej6conteostring.js
+++ ej6conteostring.js
@@ -1,19 +1,17 @@
 function conteo(numero,aumentador){
-const ourArray = [];
-for (let i = 0; i < numero; i += aumentador) {
+    const ourArray = [];
+    for (let i = 0; i < numero; i += aumentador) {
     ourArray.push(i);
+    }
+    console.log(ourArray);
 }
-console.log(ourArray)
-}
-conteo(20,4);
-
+conteo(20,2);//(A,B) cuanta hasta 'A' en multiplos de 'B' 
 //reversa
-
 function conteoR(numero,aumentador){
     const ourArray = [];
     for (let i = numero; i > 0; i -= aumentador) {
         ourArray.push(i);
     }
-    console.log(ourArray)
-    }
-    conteoR(20,4);
+    console.log(ourArray);
+}
+    conteoR(20,1);//(A,B) cuanta hasta 'A' en multiplos de 'B' en rversa
