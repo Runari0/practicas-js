441f06339167e2b212be84e3b0d53f24b3cc2a86
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-07 22:13:13 -0500

    Update ej3recursivoConteo.js
    form 1 cuanta atrás función recursivo.
    form 2 cuenta adelante sin recursividad

--- ej3recursivoConteo.js
+++ ej3recursivoConteo.js
@@ -1,4 +1,5 @@
-//? cuanta atras funcion recursivo 
+//? cuanta atras funcion recursivo
+//? form 1
 function cuentaAtras(n){
     if (n === 0){
         return console.log(n); //console.log(n) lo use para que tome el 0 en el conteo
@@ -6,4 +7,12 @@ function cuentaAtras(n){
     console.log(n);
     return cuentaAtras(n -1) //recursivo
 };
-cuentaAtras(100); // colocarlo en una linea
+cuentaAtras(10); // colocarlo en una linea
+console.log();
+//? form 2 cuenta adelante sin recursividad
+function cuenta(n){
+    for(i = 0; i <= n; ++i){
+        console.log(i);
+    }
+};
+cuenta(10); // colocarlo en una linea
